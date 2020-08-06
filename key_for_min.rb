c74f3147de52b38f1760649e5f4221ff540759d6
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 10:18:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |min_value, value|
-  if min_value == nil || value < min_value
-    min_value = value
+    if min_value == nil || value < min_value
+      min_value = value
+    end
   end
 end
